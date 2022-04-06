0a17baec845c032c49aef30e2e2ba28b06c083be
Gesugao-san <gesugao.san@gmail.com>
2022-04-06 17:24:31 +0300

    In case of dead link info

--- slashCommands/Utility/getinfo.js
+++ slashCommands/Utility/getinfo.js
@@ -41,16 +41,20 @@ module.exports = {
                 return data;
             })
             .catch((e) => {
-                if (e.message == "Unknown Invite") return {"code": inviteCode, "message": e};
+                if (e.message == "Unknown Invite" || fetchedInvite.message.code == 10006 || e.message.httpStatus == '404') {
+                    return {"code": inviteCode, "message": e};
+                }
                 return null;
         });
-        if (fetchedInvite.message == "Unknown Invite") {
+        if (fetchedInvite.message == "Unknown Invite" || fetchedInvite.message.code == 10006 || fetchedInvite.message.httpStatus == 404) {
             const embed = new client.discord.MessageEmbed()
-                .setTitle(':chains: Invite link info')
-                .addField('Invite Code', `${fetchedInvite.code}`, true)
-                .addField('Invite Status', `[${fetchedInvite.message}](https://discord.com/api/invite/${fetchedInvite.code}?with_counts=true&with_expiration=true)`, true)
-                .addField('Reason', `Provided invite link was killed or not created yet.`)
+                .setTitle(':chains: ・ Invite link info')
+                .addField('Invite Code', `[${fetchedInvite.code}](https://discord.com/api/invite/${fetchedInvite.code}?with_counts=true&with_expiration=true)`, true)
+                .addField('Invite Status', `${fetchedInvite.message}`, true) // :coffin:
+                .addField('Reason', `[Provided invite link](https://discord.gg/${fetchedInvite.code}) was deleted or was not created yet.`)
+                .addField('Possible Solution', `Ask for new one... OR: [Check WayBack Machine](https://web.archive.org/*/https://discord.gg/${fetchedInvite.code})... (ToDo)`)
                 .setColor(client.config.embedColor)
+            console.log(`[CMD] ${interaction.client.user.id} asks for invite info: ${fetchedInvite.code} (dead, ${fetchedInvite.message})`);
             return interaction.reply({ embeds: [embed] });
             //return interaction.reply({ content: `Invite link is unknown! (was killed or not created yet)` });
         }
@@ -60,22 +64,26 @@ module.exports = {
         
         const embed = new client.discord.MessageEmbed()
             .setColor(client.config.embedColor)
-            .setTitle(':chains: Invite link info')
-            .setThumbnail(fetchedInvite.guild.iconURL())
-            .addField('Invite Code', `[${fetchedInvite.code}](${fetchedInvite.url})`) //discord://-/invite/discord-testers 
+            .setTitle(':chains: ・ Invite link info') // :anchor:
+            .setThumbnail(fetchedInvite.guild !== undefined ? fetchedInvite.guild.iconURL() : "https://discord.js.org/static/djs_logo.png")
+            .addField('Invite Code, Expires at', `[${fetchedInvite.code}](${fetchedInvite.url}), ${(fetchedInvite.expiresTimestamp === 0 ? "never" : fetchedInvite.expiresTimestamp)}`) //discord://-/invite/discord-testers 
             //.addField('Invite Type', `${fetchedInvite.type}`)
-            .addField('Expires at', `${(fetchedInvite.expiresTimestamp === 0 ? "never" : fetchedInvite.expiresTimestamp)}`)
             //.addField('Invite max uses', `${fetchedInvite.maxUses}`)
-            .addField('Server: ID', `${fetchedInvite.guild.id}`)
+            .addField('Server: ID', `${fetchedInvite.guild.id}`, true)
             .addField('Name', `${fetchedInvite.guild.name}`, true)
-            .addField('Server Members: Total, Online, Offline', 
+            .addField('Members: Total, Online, Offline', 
             `${fetchedInvite.memberCount}, ${fetchedInvite.presenceCount}, ${fetchedInvite.memberCount - fetchedInvite.presenceCount}`)
-            //.addField('Server Members Online', `${fetchedInvite.presenceCount}`)
-            //.addField('Server Members Offline', `${fetchedInvite.memberCount - fetchedInvite.presenceCount}`)
+            .addField('Server description', `${fetchedInvite.guild.description}`)
+            .addField('Server vanity url code', `${fetchedInvite.vanityURLCode}`)
+            .addField('Channel', `${fetchedInvite.channel}`)
+            .addField('Channel ID', `${fetchedInvite.channelId}`)
+            .addField('Channel Name', `${fetchedInvite.channel.name}`)
             .addField('Inviter', `${fetchedInvite.inviter}`)
-            .addField('Open invite', `[Web browser](${fetchedInvite.url});\nSplash screen: <discord://-/invite/${fetchedInvite.code}>;\nDirect Go To: <discord://discord.gg/${fetchedInvite.code}>;`)
-            .addField(`Is this bot presense on server?`, `${(client.guilds.cache.get(fetchedInvite.guild.id) !== undefined ? "yes" : "no")}`)
-            .addField(`Is this bot can lookup invites on this server?`, `${(fetchedInvite.guild.me !== undefined ? (fetchedInvite.guild.me.permissions.has(Permissions.FLAGS.MANAGE_GUILD) ? "yes" : "no") : "not on server")}`); // MANAGE_GUILD
+            .addField('Open invite',
+            `[Web browser](${fetchedInvite.url}) ([API](https://discord.com/api/invite/${fetchedInvite.code}?with_counts=true&with_expiration=true));
+            Splash screen: <discord://-/invite/${fetchedInvite.code}>;\nDirect Go To: <discord://discord.gg/${fetchedInvite.code}>;`)
+            .addField(`Is this bot presense on server?`, `${(client.guilds.cache.get(fetchedInvite.guild.id) !== undefined ? "yes" : "no")}`, true)
+            .addField(`And can lookup invites here?`, `${(fetchedInvite.guild.me !== undefined ? (fetchedInvite.guild.me.permissions.has(Permissions.FLAGS.MANAGE_GUILD) ? "yes" : "no") : "I'm not on the server")}`, true); // MANAGE_GUILD
             //.setImage(fetchedInvite.guild.iconURL())
             //.setFooter({ text: `Open invite: [Web](${fetchedInvite.url}) [Splash screen](discord://-/invite/${fetchedInvite.code}) [Direct Go To](discord://discord.gg/${fetchedInvite.code})` });
 
@@ -85,7 +93,7 @@ module.exports = {
             console.log(`${client.guilds.cache.get('854664216739577867')}`);
             console.log(`${client.guilds.cache.get(toString(fetchedInvite.guild.id))}`);
             console.log(`${client.guilds.cache.get("'"+fetchedInvite.guild.id+"'")}`); */
-            //console.log(fetchedInvite);
+        //console.log(fetchedInvite);
         await interaction.reply({ embeds: [embed] });
     },
 };
