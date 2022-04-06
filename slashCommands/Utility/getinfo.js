9f17a46a123b3ad3e3b164e298be87484811360d
Gesugao-san <gesugao.san@gmail.com>
2022-04-06 09:33:50 +0300

    Update getinfo

--- slashCommands/Utility/getinfo.js
+++ slashCommands/Utility/getinfo.js
@@ -1,4 +1,12 @@
-// Example of how to make a SlashCommand
+
+async function _fetchInvite(invite) {
+    try {
+        const fetchedInvite = await client.fetchInvite(invite);
+        return fetchedInvite;
+    } catch (e) {
+        return false;
+    }
+}
 
 module.exports = {
     name: "getinfo",
@@ -15,30 +23,39 @@ module.exports = {
     description: "I will try to get information about your invite link.",
     ownerOnly: false,
     run: async (client, interaction) => {
+        if (!interaction.member.permissions.has("ADMINISTRATOR")) return interaction.reply({ content: `You can only add servers with ADMINISTRATOR authorization.` });
         const commandInt = interaction.options.getString("link");
         if (!commandInt) {
-            interaction.reply({ content: `There isn't any invite link!"` });
-            return 1;
-        } else {
-            /* try {
-                const fetchedInvite = await client.fetchInvite(commandInt) //('https://discord.gg/djs')
-            } catch (e) {
-                if (e.reason == 'Unknown Invite' || !e.ok) { //e.httpStatus == '404') {
-                    interaction.reply({ content: `Invite link is unknown! (was killed or not created yet)` });
-                } else {
-                    throw e;
-                }
-            } */
-            const fetchedInvite = await client.fetchInvite(commandInt) //('https://discord.gg/djs')
-            console.log(`Obtained invite with code: ${fetchedInvite.code}`);
-            console.log(fetchedInvite.ok);
-            const pingEmbed = new client.discord.MessageEmbed()
-                .setTitle(':chains: Invite link info')
-                .addField("Invite code", `${fetchedInvite.code}`, true)
-                .setColor(client.config.embedColor)
-                .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
-
-            await interaction.reply({ embeds: [pingEmbed] });
+            return interaction.reply({ content: `There isn't any invite link!` });
         }
+        /* try {
+            await client.fetchInvite(args);
+        } catch (err) {
+            return interaction.reply({ content: `Invite link is unknown! (was killed or not created yet)` });
+        } */
+        /* try {
+            const fetchedInvite = await client.fetchInvite(commandInt) //('https://discord.gg/djs')
+        } catch (e) {
+            if (e.reason == 'Unknown Invite' || !e.ok) { //e.httpStatus == '404') {
+                interaction.reply({ content: `Invite link is unknown! (was killed or not created yet)` });
+            } else {
+                throw e;
+            }
+        } */
+        
+        const fetchedInvite = await _fetchInvite(commandInt); //('https://discord.gg/djs')
+        if (fetchedInvite === false) {
+            return interaction.reply({ content: `Invite link is unknown! (was killed or not created yet)` });
+        }
+        console.log(`Obtained invite with code: ${fetchedInvite.code}`);
+        console.log(fetchedInvite.ok);
+        const pingEmbed = new client.discord.MessageEmbed()
+            .setTitle(':chains: Invite link info')
+            .addField("Invite code", `${fetchedInvite.code}`, true)
+            .setColor(client.config.embedColor)
+            .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
+
+        await interaction.reply({ embeds: [pingEmbed] });
+        
     },
 };
