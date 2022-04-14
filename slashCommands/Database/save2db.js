646a978a3c676f9b0793696057a5759cbd23da9c
Gesugao-san <gesugao.san@gmail.com>
2022-04-14 17:33:50 +0300

    Update save2db

--- slashCommands/Database/save2db.js
+++ slashCommands/Database/save2db.js
@@ -7,21 +7,42 @@ module.exports = {
     options: [
         {
             name: 'invite_or_ID',
-            description: 'invite (link or code), channel ID or server ID.',
+            description: 'invite (link or code), channel ID or server ID.', // ToDo: .fetchWebhook(idtoken)
             type: 'STRING',
             required: false
         }
     ],
-    description: "Not for public use, sorry.",
+    description: "I will try to fetch it and save information in my database.",
     ownerOnly: true,
     run: async (client, interaction) => {
         if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return interaction.reply({ content: `You can only add servers with ADMINISTRATOR authorization.` });
         const param1 = interaction.options.getString("invite_or_ID");
         if (!param1) return interaction.reply({ content: `There isn't any invite link, channel ID or server ID!` });
-        
-        console.log(`[CMD] ${interaction.user.id} trigger invite2button: (${fetchedInvite.code})`); //${(fetchedWidget !== undefined ? fetchedWidget.id : "widget unknown")}
+
+        if (typeof param1 !== "string") para1 = `${param1}`;
+
+        var isInvite = client.fetchInvite(param1)
+            .then((d) => {return d})
+            .catch(() => {return false});
+
+        var isChannel = message.guild.channels.fetch(param1)
+            .then((d) => {return d})
+            .catch(() => {return false});
+
+        var isServer = client.guilds.fetch(param1)
+            .then((d) => {return d})
+            .catch(() => {return false});
+
+        const saveToDBEmbed = new client.discord.MessageEmbed()
+            .setTitle('Save to DB')
+            .addField("isInvite", `${isInvite}`)
+            .addField("isChannel", `${isChannel}`)
+            .addField("isServer", `${isServer}`)
+            .setColor(client.config.embedColor);
+
+        console.log(`[CMD] ${interaction.user.id} trigger saveToDBEmbed: (${(param1 != null ? param1 : null)})`); //${(fetchedWidget !== undefined ? fetchedWidget.id : "widget unknown")}
         //console.log(fetchedWidget.channels.map((channel) => [channel.id, channel.name]));
         //console.log(fetchedWidget.channels.mapValues((channel) => [channel.id, channel.name]));
-        await interaction.reply({ embeds: [inviteToButtonInvite], components: [row] });
+        await interaction.reply({ embeds: [saveToDBEmbed] });
     },
 };
