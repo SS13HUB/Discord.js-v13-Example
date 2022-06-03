65d0f8ad568445bbb199e9f51a1207a21484e2fa
Gesugao-san <gesugao.san@gmail.com>
2022-06-03 15:49:29 +0300

    Add special-channel

---
+++ slashCommands/Utility/special-channel.js
@@ -0,0 +1,22 @@
+// Example of how to make a SlashCommand
+
+module.exports = {
+    name: "special-channel",
+    category: "Utility",
+    description: "Not for public use, sorry.",
+    ownerOnly: true,
+    run: async (client, interaction) => {
+        if (interaction.channel) {
+            await interaction.channel.sendTyping();
+        } else {
+            let _channel = await client.channels.fetch(interaction.channelId);
+            await _channel.sendTyping();
+        }
+        const pingEmbed = new client.discord.MessageEmbed()
+            .setTitle('Special message for channel')
+            .setDescription(`This channel definitely created/updated specially for me:\n${client.user.toString()}`)
+            .setColor('DARK');
+        await interaction.channel.send({ embeds: [pingEmbed] });
+        return await interaction.reply({ ephemeral: true, content: `Ok.` });
+    },
+};
